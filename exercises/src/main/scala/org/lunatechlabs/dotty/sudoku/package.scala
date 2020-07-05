2f56dbe0d5a4084074f978c1d7229775d1785974
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2020-07-05 11:48:38 -0700

    Simplified

--- exercises/src/main/scala/org/lunatechlabs/dotty/sudoku/package.scala
+++ exercises/src/main/scala/org/lunatechlabs/dotty/sudoku/package.scala
@@ -1,7 +1,8 @@
 package org.lunatechlabs.dotty
 
-package object sudoku:
+package object sudoku {
 
   implicit class RowUpdatesToSudokuField(val update: Vector[Int])
-      extends AnyVal:
-    def toSudokuField: SudokuField = ???
+    extends AnyVal {}
+
+}
